ad0dfe311d5faebc7a2488f073ab9a4747ba6597
VedikaSagdeo123 <75945402+VedikaSagdeo123@users.noreply.github.com>
2021-01-10 21:24:56 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,28 @@
+class Paper{
+constructor(x,y,radius){
+    var options={
+        isStatic:false,
+        restitution:0.3,
+        friction:0.5,
+        density:1.2
+         }
+         this.body = Bodies.circle(x,y,radius,options);
+         this.radius=radius;
+         this.height=height;
+      World.add(world, this.body);
+      
+    }
+    display(){
+        var position=this.body.position
+        var angle=this.body.angle
+        push();
+        translate(position.x,position.y)
+        rotate(angle)
+        fill("purple")
+        ellipseMode(RADIUS);
+        ellipse(0,0,this.radius,this.radius)
+        pop();
+      
+    
+    }
+}
